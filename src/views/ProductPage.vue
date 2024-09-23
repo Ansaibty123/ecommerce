5d5f82c9c44826b592cb54db037f6588f1075466
Your Name <ansaibty1@gmail.comgit config --global user.name>
2024-09-23 16:30:03 +0530

    css

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -5,10 +5,10 @@
         <img :src="product.image" alt="Product Image" class="img-fluid w-50 py-4 custom-max-width img-fit">
       </div>
       <div class="col-md-6">
-        <div class="custom-width row gap-4 gap-md-5 px-2 py-md-5">
+        <div class="custom-width row gap-2 gap-md-5 px-2 py-md-5">
           <h3>{{ product.title }}</h3>
-          <p class="fs-4">Description-<span class="text-info fs-5">{{ product.description }}</span></p>
-          <p class="fs-4 fw-bold">Price: ${{ product.price }}</p>
+          <p class="fs-md-4 fs-6">Description-<span class="text-info fs-5">{{ product.description }}</span></p>
+          <p class="fs-md-4 fs-6 fw-bold">Price: ${{ product.price }}</p>
         </div>
 
       </div>
