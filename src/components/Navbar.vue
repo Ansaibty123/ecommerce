1f3f616d2a338f9c451ca4e9a8f74feeec34e561
Your Name <ansaibty1@gmail.comgit config --global user.name>
2024-09-26 00:40:47 +0530

    css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="d-flex px-4 py-4 px-md-4 border justify-content-between">
-        <div class="fw-bold text-success px-md-3">
+    <div class="d-flex px-4 py-4 px-lg-5 border justify-content-between">
+        <div class="fw-bold text-success px-md-3 px-lg-4">
             ShopHAVEN
         </div>
-        <div class="d-flex gap-2 gap-md-3 fw-bold ">
+        <div class="d-flex gap-2 gap-md-4 gap-lg-5 fw-bold ">
             <router-link to="/about" class="text-decoration-none text-info"> About</router-link>
             <router-link to="/home" class="text-decoration-none text-info"> Home</router-link>
             <router-link to="/contact" class="text-decoration-none text-info"> Contact</router-link>
         </div>
 
-        <div class="fw-bold px-md-3">
+        <div class="fw-bold px-md-3 px-lg-4">
             <RouterLink to="/cart" class="text-decoration-none text-secondary">Cart</RouterLink>
         </div>
     </div>
