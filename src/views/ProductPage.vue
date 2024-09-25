286474b010d44a72d206f0fa9c661b21fed653ba
Your Name <ansaibty1@gmail.comgit config --global user.name>
2024-09-25 22:49:15 +0530

    push

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="product" class="container-fluid vh-100 pt-md-5">
     <div class="d-flex flex-column flex-md-row">
-      <div class="container col-md-6 text-center pt-md-3">
+      <div class="container col-md-6 text-center pt-md-3 border shadow-sm">
         <img :src="product.image" alt="Product Image" class="img-fluid w-50 py-4 custom-max-width img-fit">
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 pt-3 pt-md-0 ps-3">
         <div class="custom-width row gap-2 gap-md-5 px-2 py-md-5">
           <h3>{{ product.title }}</h3>
           <p class="fs-md-4 fs-5">Description- <span class="text-info fs-6"> {{ product.description }}</span></p>
