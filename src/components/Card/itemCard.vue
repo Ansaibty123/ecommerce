38454c0a6d4a141a84d9d2f0884dfddc5f25dcf4
Your Name <ansaibty1@gmail.comgit config --global user.name>
2024-12-13 01:01:18 +0530

    push

--- src/components/Card/itemCard.vue
+++ src/components/Card/itemCard.vue
@@ -2,7 +2,7 @@
     <div @click="goToProduct(item.id)" class="p-2 p-md-3 d-flex 
     flex-column align-items-center justify-content-between
      shadow-lg border-success my-3 my-md-4">
-        <div class="custom-image p-1 py-2">
+        <div class="custom-image ">
             <img :src="item.image" alt="" class="img-fluid custom-img">
         </div>
         <div class="custom-text fs-6 p-1 text-white text-start">
@@ -41,7 +41,7 @@ export default {
 .custom-image {
     height: 100px;
     width: 100px;
-    box-shadow: 0 4px 8px rgba(255, 255, 0, 0.7); 
+    box-shadow: 0 2px 14px yellow; 
     border-radius: 10px; 
 
 }
@@ -71,7 +71,7 @@ export default {
   font-size: 20px;
 }
 .shadow-lg {
-    box-shadow: 0 4px 8px rgba(255, 255, 255, 0.8) !important; /* White shadow */
+    box-shadow: 0 0px 14px rgba(255, 255, 255, 0.8) !important; /* White shadow */
 }
 
 @media(min-width:768px) {
